72727b28e796b08dcaccef17ac89e5f9244750bc
Daniel Custodio <play.daniel@hotmail.com>
2022-07-04 16:44:52 -0300

    feat: Criação da função de leitura de .csv

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ---------
-    path : str
-        Full path to file
-
-    Returns
-    ------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, mode="r", encoding="utf8") as jobs_file:
+        list_all = []
+        jobs_list = csv.DictReader(jobs_file, delimiter=",", quotechar='"')
+        for job in jobs_list:
+            list_all.append(job)
+        return list_all
