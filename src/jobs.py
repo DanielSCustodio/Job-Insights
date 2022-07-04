3fddfe73ca1cb98bccd6e915bd5d093f7aa37130
Daniel Custodio <play.daniel@hotmail.com>
2022-07-04 14:34:15 -0300

    Start :rocket:

--- src/jobs.py
+++ src/jobs.py
@@ -6,12 +6,12 @@ def read(path):
     """Reads a file from a given path and returns its contents
 
     Parameters
-    ----------
+    ---------
     path : str
         Full path to file
 
     Returns
-    -------
+    ------
     list
         List of rows as dicts
     """
