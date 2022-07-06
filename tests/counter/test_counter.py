f8de725cde7fc8f789e1760383a97e1b84e67e30
Daniel Custodio <play.daniel@hotmail.com>
2022-07-05 22:33:41 -0300

    feat: Função de teste count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    total_senior = count_ocurrences("src/jobs.csv", "senior")
+
+    assert count_ocurrences("src/jobs.csv", "Senior") == count_ocurrences(
+        "src/jobs.csv", "senior")
+
+    assert total_senior == 1791
